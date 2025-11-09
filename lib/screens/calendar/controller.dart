028fe98b70a596b04b9783279b7d835bc39c055f
SergeyFierce <102462393+SergeyFierce@users.noreply.github.com>
2025-11-09 17:15:59 +0300

    Revert "Revert "Create modular shell with animated navigation""

---
+++ lib/screens/calendar/controller.dart
@@ -0,0 +1,4 @@
+class CalendarController {
+  String get headline => 'Календарь';
+  String get description => 'Планируйте свои события и отслеживайте ближайшие задачи.';
+}
---
+++ lib/screens/settings/controller.dart
@@ -0,0 +1,4 @@
+class SettingsController {
+  String get headline => 'Настройки';
+  String get description => 'Персонализируйте приложение под свои привычки и предпочтения.';
+}
